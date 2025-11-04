6fd4b1df0f8c92a4b6bd6cca9a7eda6e0411abcb
Rafael Ferrer <rferrervera@gmail.com>
2025-11-04 13:21:30 -0800

    Created connection with Virtual device and tested movement. Added Free movement window without functionality.

--- Controller.py
+++ Controller.py
@@ -1,6 +1,4 @@
 from PyQt6.QtCore import QObject, pyqtSignal
-import Hardware
-import Constants
 from Constants import SERIAL_PORT
 from Hardware import XYStage
 
@@ -14,4 +12,17 @@ class StageController(QObject):
         super().__init__()
         self.stage = XYStage(port)
 
+    def stage_home(self):
+        self.stage.home_all_axes()
 
+    def stage_stop(self):
+        self.stage.stop_move()
+
+    def stage_move(self, pos: float, vel: float):
+        self.stage.move_to(pos, vel)
+
+    def stage_is_moving(self):
+        if self.stage.connection.is_busy():
+            return True
+        else:
+            return False
